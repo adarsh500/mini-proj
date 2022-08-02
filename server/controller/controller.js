f52f870d0c113b49e99250383ed5ba181690edcd
adarsh50 <adarshsulegai50@gmail.com>
2022-08-03 00:44:31 +0530

    chore: add cors and fix lint

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,168 +1,166 @@
-var CriminalDB = require('../models/criminal')
-var CrimeDB = require('../models/cime')
-var FirDB = require('../models/fir')
-var VictimDB = require('../models/victim')
+var CriminalDB = require('../models/criminal');
+var CrimeDB = require('../models/cime');
+var FirDB = require('../models/fir');
+var VictimDB = require('../models/victim');
 
+//create new criminal record
+exports.createcriminal = (req, res) => {
+  //validate request
 
-//create new criminal record 
-exports.createcriminal = (req,res) => {
-    //validate request
-    if(!req.body){
-        res.status(400).send({message : "Content cannt be empty"})
-        return;
-    }
+  if (!req.body) {
+    res.status(400).send({ message: 'Content cannt be empty' });
+    return;
+  }
 
-    //new criminal 
-    const criminal = new CriminalDB({
-        id: req.body.id,
-        name: req.body.name,
-        email: req.body.email,
-        adhaar: req.body.adhaar,
-        address: req.body.address
-    })
+  //new criminal
+  const criminal = new CriminalDB({
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+    adhaar: req.body.adhaar,
+    address: req.body.address,
+  });
 
-    //save criminal in db
-    criminal
+  //save criminal in db
+  criminal
     .save(criminal)
-    .then(data =>{
-        res.send(data)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error while creating criminal record"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error while creating criminal record',
+      });
+    });
+};
 
-//create fir 
-exports.createfir = (req,res) => {
-    //validate request
-    if(!req.body){
-        res.status(400).send({message : "Content cannt be empty"})
-        return;
-    }
+//create fir
+exports.createfir = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: 'Content cannt be empty' });
+    return;
+  }
 
-    const Fir = new FirDB({
-        id: req.body.id,
-        description: req.body.description,
-        type: req.body.type
-    })
+  const Fir = new FirDB({
+    id: req.body.id,
+    description: req.body.description,
+    type: req.body.type,
+  });
 
-    Fir
-    .save(Fir)
-    .then(data =>{
-        res.send(data)
+  Fir.save(Fir)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error while creating FIR record"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error while creating FIR record',
+      });
+    });
+};
 
 //create crime record
-exports.createcrime = (req,res) => {
-    //validate request
-    if(!req.body){
-        res.status(400).send({message : "Content cannt be empty"})
-        return;
-    }
+exports.createcrime = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: 'Content cannt be empty' });
+    return;
+  }
 
-    const Crime = new CrimeDB({
-        id: req.body.id,
-        name: req.body.name,
-        type: req.body.type,
-        location: req.body.location,
-        date: req.body.date,
-        adhaar: req.body.adhaar,
-        address: req.body.address
+  const Crime = new CrimeDB({
+    id: req.body.id,
+    name: req.body.name,
+    type: req.body.type,
+    location: req.body.location,
+    date: req.body.date,
+    adhaar: req.body.adhaar,
+    address: req.body.address,
+  });
+
+  Crime.save(Crime)
+    .then((data) => {
+      res.send(data);
     })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error while creating CRIME record',
+      });
+    });
+};
 
-    Crime
-    .save(Crime)
-    .then(data =>{
-        res.send(data)
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error while creating CRIME record"
-        })
-    })
-}
+//create victim record
+exports.createvictim = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: 'Content cannt be empty' });
+    return;
+  }
 
-//create victim record 
-exports.createvictim = (req,res) => {
-    //validate request
-    if(!req.body){
-        res.status(400).send({message : "Content cannt be empty"})
-        return;
-    }
+  const victim = new VictimDB({
+    id: req.body.id,
+    name: req.body.name,
+    email: req.body.email,
+    adhaar: req.body.adhaar,
+    address: req.body.address,
+  });
 
-    const victim = new VictimDB({
-        id: req.body.id,
-        name: req.body.name,
-        email: req.body.email,
-        adhaar: req.body.adhaar,
-        address: req.body.address
-    })
-
-    victim
+  victim
     .save(victim)
-    .then(data =>{
-        res.send(data)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error while creating VICTIM record"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Some error while creating VICTIM record',
+      });
+    });
+};
 
 //get all firs
-exports.findAllFirs = (req,res) => {
-    FirDB.find()
-    .then(data => {
-        res.send(data)
+exports.findAllFirs = (req, res) => {
+  FirDB.find()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err=>{
-        res.status(500).send({
-            message: err.message || "Error occured while retrieving fir data"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error occured while retrieving fir data',
+      });
+    });
+};
 //get all criminals
-exports.findAllcriminals = (req,res) => {
-    CriminalDB.find()
-    .then(data => {
-        res.send(data)
+exports.findAllcriminals = (req, res) => {
+  CriminalDB.find()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err=>{
-        res.status(500).send({
-            message: err.message || "Error occured while retrieving fir data"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error occured while retrieving fir data',
+      });
+    });
+};
 //get all Crime record
-exports.findAllCrime = (req,res) => {
-    CrimeDB.find()
-    .then(data => {
-        res.send(data)
+exports.findAllCrime = (req, res) => {
+  CrimeDB.find()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err=>{
-        res.status(500).send({
-            message: err.message || "Error occured while retrieving fir data"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error occured while retrieving fir data',
+      });
+    });
+};
 //get all Victim record
-exports.findAllVictim = (req,res) => {
-    VictimDB.find()
-    .then(data => {
-        res.send(data)
+exports.findAllVictim = (req, res) => {
+  VictimDB.find()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err=>{
-        res.status(500).send({
-            message: err.message || "Error occured while retrieving fir data"
-        })
-    })
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error occured while retrieving fir data',
+      });
+    });
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 const connectDB = require('./database/connection');
+const cors = require('cors');
 dotenv.config();
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+app.use(cors({ origin: '*' }));
 
 //DB connection
 connectDB();
@@ -17,14 +19,14 @@ app.use(bodyparser.json());
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// app.get('/', (req, res) => res.send('hello there'));
+app.get('/', (req, res) => res.send('hello there'));
 
 // app.get('/api', (req, res) => {
 //   res.json({ message: 'Hello from OBI WAN KENOBI!' });
 // });
 
 //Adding routers
-app.use('/',require('./routes/router'))
+app.use('/', require('./routes/router'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
