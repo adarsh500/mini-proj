23b36abe94f3c79d43660bb8edee28a8c86c68b2
adarsh50 <adarshsulegai50@gmail.com>
2022-09-07 17:01:08 +0530

    add handlers for update and delete

--- client/src/components/FirTable.jsx
+++ client/src/components/FirTable.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
+import FirModal from './FirModal';
 
 import { Link } from 'react-router-dom';
 import { Button, Table, Form, Modal } from 'react-bootstrap';
 
 const FirTable = () => {
   const [fir, setFir] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
     fetch('/api/fir')
@@ -15,29 +18,48 @@ const FirTable = () => {
       });
   }, []);
 
+  const handleDelete = (id) => {};
+
+  const handleEdit = (id) => {};
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Description</th>
-          <th>Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        {fir.map((f, index) => (
-          <tr key={index}>
-            <td>
-              <Link to={`/${f.id}`} key={f.id}>
-                {f.id}
-              </Link>
-            </td>
-            <td>{f.description}</td>
-            <td>{f.type}</td>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>Delete</th>
+            <th>Edit</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {fir.map((f, index) => (
+            <tr key={index}>
+              <td>
+                <Link to={`/${f.id}`} key={f.id}>
+                  {f.id}
+                </Link>
+              </td>
+              <td>{f.description}</td>
+              <td>{f.type}</td>
+              <td>
+                <Button variant="danger" onClick={handleDelete(f.id)}>
+                  Delete
+                </Button>
+              </td>
+              <td>
+                <Button variant="secondary" onClick={handleEdit(f.id)}>
+                  Edit
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <FirModal show={modalShow} onHide={() => setModalShow(false)} />
+    </>
   );
 };
 
